23e1c9a002d0e14a14ccb004b1311dcdb35cdac9
IvanBrasilico <brasilico.ivan@gmail.com>
2018-03-21 12:15:03 -0300

    metricas

--- padma/gym/treinador_pesagem.py
+++ padma/gym/treinador_pesagem.py
@@ -1,12 +1,24 @@
+"""Permite treinar o modelo definido em models/peso/peso.py.
+
+Precisa de um csv no formato:
+    _id,numero,peso,volume
+    sendo _id o ObjectId da imagem no GridFS
+
+Uma vez treinado, salva os histogramas e os pesos(em labels)
+Para treinar novamente, basta excluir arquivos .pkl
+
+"""
 import csv
 import numpy as np
 import os
+# TODO: passar tudo para numpy e eliminar pickle
 import pickle
 from bson.objectid import ObjectId
 from gridfs import GridFS
 from PIL import Image
 from pymongo import MongoClient
 from sklearn import linear_model
+from sklearn.metrics import explained_variance_score, mean_absolute_error
 from scipy import misc
 from ajna_commons.flask.conf import (DATABASE, MONGODB_URI)
 from padma.models.peso.peso import PesoModel
@@ -32,9 +44,9 @@ def make_histograms():
         id_index = linha.index('id')
         peso_index = linha.index('peso')
         for ind, linha in enumerate(reader):
-            print('.', end='')
-            if ind // 100 == 0:
+            if ind % 100 == 0:
                 print(ind)
+            print('.', end='', flush=True)
             grid_out = fs.get(ObjectId(linha[id_index]))
             tempfile = '/tmp/temp.jpg'
             with open(tempfile, 'wb') as temp:
@@ -42,7 +54,9 @@ def make_histograms():
             image = Image.open(tempfile)
             im = np.asarray(image)
             histograms.append(modelclass.hist(im))
-            labels.append(linha[peso_index])
+            labels.append(float(linha[peso_index]))
+            # print(histograms)
+            # print(labels)
         with open(HIST_FILE, 'wb') as out:
             pickle.dump(histograms, out)
         with open(LABEL_FILE, 'wb') as out:
@@ -55,10 +69,19 @@ def load_histograms():
         histograms = pickle.load(pkl)
     with open(LABEL_FILE, 'rb') as pkl:
         labels = pickle.load(pkl)
+    return histograms, labels
 
 
 if __name__ == '__main__':
     if not os.path.exists(HIST_FILE):
         make_histograms()
     else:
-        load_histograms()
+        histograms, labels = load_histograms()
+    histograms_train = histograms[:800]
+    labels_train = labels[:800]
+    histograms_test = histograms[-200:]
+    labels_test = labels[-200:]
+    modelclass.train(histograms_train, labels_train)
+    labels_predicted = modelclass.model.predict(histograms_test)
+    print(explained_variance_score(labels_test, labels_predicted))
+    print(mean_absolute_error(labels_test, labels_predicted))
