12a4a7fd7e1d820c6814b16ddf553a513e6d45c4
IvanBrasilico <brasilico.ivan@gmail.com>
2019-03-29 18:19:43 -0300

    --

--- padma/models/conteiner20e40/bbox.py
+++ padma/models/conteiner20e40/bbox.py
@@ -45,6 +45,8 @@ def create_graph():
 
 
 def load_image_into_numpy_array(image):
+    if image.size[0] > 600:
+        image = image.resize(SIZE, Image.ANTIALIAS)
     (im_width, im_height) = image.size
     return np.array(image.getdata()).reshape(
         (im_height, im_width, 3)).astype(np.uint8)
